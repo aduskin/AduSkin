e555f10127c397289d97d97205d10525275bce2c
AduSkin <473500619@qq.com>
2020-03-03 08:56:18 +0800

    修复日期控件本地时间问题

--- src/AduSkin.Demo/ViewModel/ModuleViewModel/AduSkinSupportViewModel.cs
+++ src/AduSkin.Demo/ViewModel/ModuleViewModel/AduSkinSupportViewModel.cs
@@ -17,7 +17,7 @@ namespace AduSkin.Demo.ViewModel
          AllSupports.Add(new SupportUser("不愿意透露名字的网友", "tencent://message/?uin=870856195&Site=&Menu=yes", "http://q.qlogo.cn/headimg_dl?bs=qq&dst_uin=870856195&spec=100","",new string[] { SupportType.Money.ToString()+"：100元" }));
          AllSupports.Add(new SupportUser("沙漠尽头的狼 dotnet9.com", "tencent://message/?uin=632871194&Site=&Menu=yes", "http://q.qlogo.cn/headimg_dl?bs=qq&dst_uin=632871194&spec=100", "", new string[] { SupportType.Skill.ToString(), SupportType.Extension.ToString() }));
          AllSupports.Add(new SupportUser("关关", "tencent://message/?uin=2453966523&Site=&Menu=yes", "http://q.qlogo.cn/headimg_dl?bs=qq&dst_uin=2453966523&spec=100", "", new string[] { SupportType.Skill.ToString(), SupportType.Extension.ToString() }));
-         AllSupports.Add(new SupportUser("Tom", "tencent://message/?uin=17379620&Site=&Menu=yes", "http://q.qlogo.cn/headimg_dl?bs=qq&dst_uin=17379620&spec=100", "", new string[] { SupportType.Skill.ToString(), SupportType.Extension.ToString() }));
+         AllSupports.Add(new SupportUser("Tom", "tencent://message/?uin=17379620&Site=&Menu=yes", "http://q.qlogo.cn/headimg_dl?bs=qq&dst_uin=17379620&spec=100", "", new string[] { SupportType.Skill.ToString(), SupportType.Extension.ToString(),SupportType.Money.ToString() + "：350元" }));
       }
       private ObservableCollection<SupportUser> _AllSupports =new ObservableCollection<SupportUser>();
       /// <summary>
