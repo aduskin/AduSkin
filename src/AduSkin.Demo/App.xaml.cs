5230d766959020c822f11bd6106be9e96cda0057
John0King <ABlueSky@outlook.com>
2020-04-06 01:23:10 +0800

    修复拼音筛选

--- src/AduSkin.Demo/App.xaml.cs
+++ src/AduSkin.Demo/App.xaml.cs
@@ -3,20 +3,28 @@ using System.Windows;
 
 namespace AduSkin.Demo
 {
-   public partial class App : Application
-   {
-      protected override void OnStartup(StartupEventArgs e)
-      {
-         var splashScreen = new SplashScreen("Resources/aduskin.png");
-         splashScreen.Show(true);
-         base.OnStartup(e);
-         //初始化通知弹框
-         NoticeManager.Initialize();
-      }
-      protected override void OnExit(ExitEventArgs e)
-      {
-         NoticeManager.ExitNotifiaction();
-         base.OnExit(e);
-      }
-   }
+    public partial class App : Application
+    {
+        public App()
+        {
+#if !NET45
+            // start from net46 , you need to register text encoding provider first to use it!
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+#endif
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var splashScreen = new SplashScreen("Resources/aduskin.png");
+            splashScreen.Show(true);
+            base.OnStartup(e);
+            //初始化通知弹框
+            NoticeManager.Initialize();
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            NoticeManager.ExitNotifiaction();
+            base.OnExit(e);
+        }
+    }
 }
