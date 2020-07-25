0556b9e93d8bdb82fd2a05f123cfd98be9fc614e
AduSkin <473500619@qq.com>
2020-07-25 13:21:29 +0800

    赞助支持

--- src/AduSkin/Controls/Converter/BoolToVisibility.cs
+++ src/AduSkin/Controls/Converter/BoolToVisibility.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
@@ -13,6 +13,22 @@ namespace AduSkin.Controls.Converter
             return Visibility.Visible;
          else
             return Visibility.Collapsed;
+      }
+
+      public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+      {
+         throw new NotImplementedException();
+      }
+   }
+
+   public class BoolToReVisibility : IValueConverter
+   {
+      public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+      {
+         if ((bool)value)
+            return Visibility.Collapsed;
+         else
+            return Visibility.Visible;
       }
 
       public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
