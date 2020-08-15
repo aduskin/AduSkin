258ad2f5048d8715f672addad44342e6f9d93de5
Alfur <chenqibuu@163.com>
2020-08-16 00:51:16 +0800

    1.修改瀑布流布局的加载方式 2.修改demo中瀑布流的使用

--- src/AduSkin/Controls/Metro/MetroWaterfallFlow.cs
+++ src/AduSkin/Controls/Metro/MetroWaterfallFlow.cs
@@ -1,138 +1,97 @@
-﻿using AduSkin.Utility.Element;
+using AduSkin.Utility.Element;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace AduSkin.Controls.Metro
 {
-    public class MetroWaterfallFlow : Canvas
-    {
-        int column;
-        double listWidth = 200;
-        public double ListWidth { get { return listWidth; } set { listWidth = value; SetColumn(); } }
-        static MetroWaterfallFlow()
-        {
-            ElementBase.DefaultStyle<MetroWaterfallFlow>(DefaultStyleKeyProperty);
-        }
-        public MetroWaterfallFlow()
-        {
-            Loaded += delegate
-            {
-                SetColumn();
-                Margin = new Thickness(Margin.Left);
-            };
-            SizeChanged += delegate
-            {
-                SetColumn();
-            };
-        }
+   public class MetroWaterfallFlow : Panel
+   {
+      private int _colums = 0;
+      private double _actualListWidth;
+      private double _maxheight;
+      private double _panelWidth;
+      private double[] _curHeight;
 
-        void SetColumn()
-        {
-            // MinWidth = listWidth + Margin.Left * 4;
-            column = (int)(ActualWidth / listWidth);
-            if (column <= 0) column = 1;
-            Refresh();
-        }
+      public double ListWidth { get; set; } = 200;
+      static MetroWaterfallFlow()
+      {
+         ElementBase.DefaultStyle<MetroWaterfallFlow>(DefaultStyleKeyProperty);
+      }
+      public MetroWaterfallFlow()
+      {
 
-        public void Add(FrameworkElement element)
-        {
-            element.Width = ListWidth;
-            if (element is Grid)
+      }
+
+      protected override Size MeasureOverride(Size constraint)
+      {
+         if (double.IsInfinity(constraint.Width))
+         {
+            _colums = InternalChildren.Count;
+            _actualListWidth = ListWidth;
+            _panelWidth = _actualListWidth * _colums;
+         }
+         else
+         {
+            _colums = (int)Math.Floor(constraint.Width / ListWidth);
+            _actualListWidth = constraint.Width / _colums;
+            _panelWidth = constraint.Width;
+         }
+         _curHeight = new double[_colums];
+         foreach (UIElement item in InternalChildren)
+         {
+            var col = GetFistMiniHeight();
+            item.SetValue(WidthProperty, _actualListWidth);
+            item.Measure(new Size(_actualListWidth, double.PositiveInfinity));
+            SetCurHeight(col, item.DesiredSize.Height);
+         }
+         return new Size(_panelWidth, _maxheight);
+      }
+
+      protected override Size ArrangeOverride(Size arrangeSize)
+      {
+         _curHeight = new double[_colums];
+         for (int i = 0; i < InternalChildren.Count; i++)
+         {
+            var item = InternalChildren[i];
+            var col = GetFistMiniHeight();
+            var rect = new Rect(_actualListWidth * col, _curHeight[col], _actualListWidth, item.DesiredSize.Height);
+            SetCurHeight(col, item.DesiredSize.Height);
+            item.Arrange(rect);
+         }
+         return base.ArrangeOverride(arrangeSize);
+      }
+
+      /// <summary>
+      /// 更新当前的高度数组,并设置面板的高度
+      /// </summary>
+      /// <param name="i"></param>
+      /// <param name="value"></param>
+      private void SetCurHeight(int i, double value)
+      {
+         _curHeight[i] += value;
+         _maxheight = Math.Max(_curHeight[i], _maxheight);
+      }
+
+      /// <summary>
+      /// 获取当前高度最低列
+      /// </summary>
+      /// <returns></returns>
+      private int GetFistMiniHeight()
+      {
+         int ret = -1;
+         double min = _curHeight.Last();
+         for (int i = _colums - 1; i >= 0; i--)
+         {
+            if (_curHeight[i] <= min)
             {
-                if ((element as Grid).Children.Count > 0)
-                {
-                    ((element as Grid).Children[0] as FrameworkElement).Margin = new Thickness(Margin.Left);
-                }
+               min = _curHeight[i];
+               ret = i;
             }
-            Children.Add(element);
-            Refresh();
-        }
-
-        public class Point
-        {
-            public int Index { get; set; }
-            public double Buttom { get; set; }
-            public double Height { get; set; }
-            public Point(int index, double height, double buttom) { Index = index;Height = height; Buttom = buttom; }
-        }
-
-        public void Refresh()
-        {
-            // 初始化参数
-            var maxHeight = 0.0;
-            var list = new Dictionary<int, Point>();
-            var nlist = new Dictionary<int, Dictionary<int, Point>>();
-            for (int i = 0; i < Children.Count; i++)
-            {
-                (Children[i] as FrameworkElement).UpdateLayout();
-                list.Add(i, new Point(i,(Children[i] as FrameworkElement).ActualHeight, 0.0));
-            }
-            for (int i = 0; i < column; i++)
-            {
-                nlist.Add(i, new Dictionary<int, Point>());
-            }
-
-            // 智能排序到 nlist
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (i < column)
-                {
-                    list[i].Buttom = list[i].Height;
-                    nlist[i].Add(nlist[i].Count, list[i]);
-                }
-                else
-                {
-                    var b = 0.0;
-                    var l = 0;
-                    for (int j = 0; j < column; j++)
-                    {
-                        var jh = nlist[j][nlist[j].Count - 1].Buttom + list[i].Height;
-                        if (b == 0.0 || jh < b)
-                        {
-                            b = jh;
-                            l = j;
-                        }
-                    }
-                    list[i].Buttom = b;
-                    nlist[l].Add(nlist[l].Count, list[i]);
-                }
-            }
-
-            // 开始布局
-            for (int i = 0; i < nlist.Count; i++)
-            {
-                for (int j = 0; j < nlist[i].Count; j++)
-                {
-                    Children[nlist[i][j].Index].SetValue(LeftProperty, i * ActualWidth / column);
-                    Children[nlist[i][j].Index].SetValue(TopProperty, nlist[i][j].Buttom - nlist[i][j].Height);
-                    Children[nlist[i][j].Index].SetValue(WidthProperty, ActualWidth / column);
-
-                    if (Children[nlist[i][j].Index] is Grid)
-                    {
-                        ((Children[nlist[i][j].Index] as Grid).Children[0] as FrameworkElement).Margin = Margin;
-                    }
-                }
-
-                // 不知道为什么如果不写这么一句会出错
-                if (nlist.ContainsKey(i))
-                {
-                    if (nlist[i].ContainsKey(nlist[i].Count - 1))
-                    {
-                        var mh = nlist[i][nlist[i].Count - 1].Buttom;
-                        maxHeight = mh > maxHeight ? mh : maxHeight;
-                    }
-                }
-            }
-            Height = maxHeight;
-            list.Clear();
-            nlist.Clear();
-        }
-
-        public void Remove(UIElement element)
-        {
-            Children.Remove(element);
-            Refresh();
-        }
-    }
+         }
+         return ret;
+      }
+   }
 }
