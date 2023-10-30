7f0d99ec367c4cbede75bc3dbab86cffa321e11b
AduSkin <473500619@qq.com>
2023-10-31 00:31:23 +0800

    .

--- src/AduSkin/Controls/Metro/AduFormLable.cs
+++ src/AduSkin/Controls/Metro/AduFormLable.cs
@@ -7,23 +7,45 @@ namespace AduSkin.Controls.Metro
 {
    public class AduFormLable : Label
    {
-      //标题
+      /// <summary>
+      /// 标题
+      /// </summary>
       public static readonly DependencyProperty TitleProperty = ElementBase.Property<AduFormLable, string>("TitleProperty", "");
-      //标题宽度
+      /// <summary>
+      /// 标题宽度
+      /// </summary>
       public static readonly DependencyProperty TitleWidthProperty = ElementBase.Property<AduFormLable, double>("TitleWidthProperty");
-      //标题最小宽度
+      /// <summary>
+      /// 标题最小宽度
+      /// </summary>
       public static readonly DependencyProperty TitleMinWidthProperty = ElementBase.Property<AduFormLable, double>("TitleMinWidthProperty");
-      //标题靠左右
+      /// <summary>
+      /// 标题靠左右
+      /// </summary>
       public static readonly DependencyProperty TitleHorizontalAlignmentProperty = ElementBase.Property<AduFormLable, HorizontalAlignment>("TitleHorizontalAlignmentProperty", HorizontalAlignment.Center);
-      //标题靠上下
+      /// <summary>
+      /// 标题内边距
+      /// </summary>
+      public static readonly DependencyProperty TitlePaddingProperty = ElementBase.Property<AduFormLable, Thickness>("TitlePaddingProperty");
+      /// <summary>
+      /// 标题靠上下
+      /// </summary>
       public static readonly DependencyProperty TitleVerticalAlignmentProperty = ElementBase.Property<AduFormLable, VerticalAlignment>("TitleVerticalAlignmentProperty", VerticalAlignment.Center);
-      //标题颜色
+      /// <summary>
+      /// 标题颜色
+      /// </summary>
       public static readonly DependencyProperty TitleForegroundProperty = ElementBase.Property<AduFormLable, Brush>("TitleForegroundProperty");
-      //错误信息
+      /// <summary>
+      /// 错误信息
+      /// </summary>
       public static readonly DependencyProperty ErrorMessageProperty = ElementBase.Property<AduFormLable, string>("ErrorMessageProperty", "");
-      //错误颜色
+      /// <summary>
+      /// 错误颜色
+      /// </summary>
       public static readonly DependencyProperty ErrorForegroundProperty = ElementBase.Property<AduFormLable, Brush>("ErrorForegroundProperty");
-      //显示错误信息
+      /// <summary>
+      /// 显示错误信息
+      /// </summary>
       public static readonly DependencyProperty ErrorVisibilityProperty = ElementBase.Property<AduFormLable, Visibility>("ErrorVisibilityProperty", Visibility.Collapsed);
 
       public string Title { get { return (string)GetValue(TitleProperty); } set { SetValue(TitleProperty, value); } }
@@ -31,6 +53,7 @@ namespace AduSkin.Controls.Metro
       public double TitleWidth { get { return (double)GetValue(TitleWidthProperty); } set { SetValue(TitleWidthProperty, value); } }
       public HorizontalAlignment TitleHorizontalAlignment { get { return (HorizontalAlignment)GetValue(TitleHorizontalAlignmentProperty); } set { SetValue(TitleHorizontalAlignmentProperty, value); } }
       public VerticalAlignment TitleVerticalAlignment { get { return (VerticalAlignment)GetValue(TitleVerticalAlignmentProperty); } set { SetValue(TitleVerticalAlignmentProperty, value); } }
+      public Thickness TitlePadding { get { return (Thickness)GetValue(TitlePaddingProperty); } set { SetValue(TitlePaddingProperty, value); } }
       public Brush TitleForeground { get { return (Brush)GetValue(TitleForegroundProperty); } set { SetValue(TitleForegroundProperty, value); } }
       public string ErrorMessage { get { return (string)GetValue(ErrorMessageProperty); } set { SetValue(ErrorMessageProperty, value); } }
       public Brush ErrorForeground { get { return (Brush)GetValue(ErrorForegroundProperty); } set { SetValue(ErrorForegroundProperty, value); } }
