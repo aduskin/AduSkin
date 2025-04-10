cf57cf16bdd11e123b85d5d9394a89a70921862c
AduSkin <473500619@qq.com>
2025-04-10 14:15:45 +0800

    a

--- src/AduSkin/Controls/Metro/MetroContextMenu.cs
+++ src/AduSkin/Controls/Metro/MetroContextMenu.cs
@@ -1,15 +1,10 @@
-﻿using AduSkin.Utility.Element;
+using AduSkin.Utility.Element;
 using System.Windows.Controls;
 
 namespace AduSkin.Controls.Metro
 {
     public class MetroContextMenu : ContextMenu
     {
-        public MetroContextMenu()
-        {
-            
-        }
-
         static MetroContextMenu()
         {
             ElementBase.DefaultStyle<MetroContextMenu>(DefaultStyleKeyProperty);
